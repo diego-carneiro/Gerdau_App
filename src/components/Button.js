38b69672ba36e8fd4ab41c6b6f2c16719dc25a7a
diego-carneiro <diegosantos.cs@gmail.com>
2022-05-12 16:39:16 -0300

    missing work

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import { Shadow } from "react-native-neomorph-shadows";
 
 export default function Button({ name }) {
   return (
-    <View style={styles.header}>
-      <Text style={{ color: "#FFF", fontSize: 30}}>{name}</Text>
-    </View>
+
+      <View style={styles.button}>
+        <Text style={{ color: "#FFF", fontSize: 30 }}>{name}</Text>
+      </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-  header: {
+  button: {
     height: 72,
     backgroundColor: "#1E97F3",
     margin: 12,
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 10,
+    borderRadius: 50,
+    elevation: 8,
   },
 });
